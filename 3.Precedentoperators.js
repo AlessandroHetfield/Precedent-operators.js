24f37a4fd8deceaaa1ad929d8cfc7a40287d1f62
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-04-10 18:10:17 -0400

    Add files via upload

---
+++ 3.Precedentoperators.js
@@ -0,0 +1,23 @@
+// Comparison operators
+console.log (ageJonas > ageSarah);
+console.log (ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18 ;
+console.log(now - 1991 > now - 2018 );
+
+
+// Precedent Operators
+
+const now= 2037;
+const ageJonas = now-1991;
+const ageSarah = now - 2018;
+
+console.log ( now - 1991 > now - 2018 );
+
+let y,z;
+y=z=25-10-5;
+console.log (z,y);
+const averageAge = (ageJonas + ageSarah) / 2
+console.log (ageJonas, ageSarah, ageJonas + ageSarah, averageAge);
+/*BMI = mass / height ** 2 = mass / (height * height) 
+(mass in kg   and height in meter).*/
